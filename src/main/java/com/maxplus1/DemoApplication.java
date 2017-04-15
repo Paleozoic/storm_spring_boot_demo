851c141a5a1e58e6a76a50303279927a376ca1aa
DESKTOP-KMR7RKV\qxloo <573793638@qq.com>
2017-04-15 13:04:10 +0800

    update Main Class

--- src/main/java/com/maxplus1/DemoApplication.java
+++ src/main/java/com/maxplus1/DemoApplication.java
@@ -1,13 +1,9 @@
 package com.maxplus1;
 
-import com.maxplus1.demo.storm.props.StormProps;
-import org.apache.storm.Config;
-import org.apache.storm.LocalCluster;
-import org.apache.storm.StormSubmitter;
+import com.maxplus1.demo.storm.AppMain;
 import org.apache.storm.generated.AlreadyAliveException;
 import org.apache.storm.generated.AuthorizationException;
 import org.apache.storm.generated.InvalidTopologyException;
-import org.apache.storm.topology.TopologyBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -19,35 +15,10 @@ public class DemoApplication {
     public static void main(String[] args)
             throws InvalidTopologyException, AuthorizationException, AlreadyAliveException, InterruptedException {
         ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-        StormProps stormProps = context.getBean(StormProps.class);
-        TopologyBuilder topologyBuilder = context.getBean(TopologyBuilder.class);
-        Config config = new Config();
-        remoteSubmit(stormProps,topologyBuilder,config);
-//        localSubmit(stormProps.getTopologyName(),topologyBuilder,config);
-//        context.close();
+        AppMain appMain = context.getBean(AppMain.class);
+        appMain.Laugher();
         SpringApplication.exit(context);
     }
 
-    private static void remoteSubmit(StormProps stormProps,TopologyBuilder builder, Config conf)
-            throws InvalidTopologyException, AuthorizationException, AlreadyAliveException {
-        conf.setNumWorkers(stormProps.getTopologyWorkers());
-        conf.setMaxSpoutPending(stormProps.getTopologyMaxSpoutPending());
-        StormSubmitter.submitTopology(stormProps.getTopologyName(), conf, builder.createTopology());
-    }
 
-    /**
-     * 用于debug
-     * @param name
-     * @param builder
-     * @throws InterruptedException
-     */
-    private static void localSubmit(String name,TopologyBuilder builder, Config conf)
-            throws InterruptedException {
-        conf.setDebug(true);
-        conf.setMaxTaskParallelism(3);
-        LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology(name, conf, builder.createTopology());
-        Thread.sleep(100000);
-        cluster.shutdown();
-    }
 }
---
+++ src/main/java/com/maxplus1/demo/storm/AppMain.java
@@ -0,0 +1,54 @@
+package com.maxplus1.demo.storm;
+
+import com.maxplus1.demo.storm.props.StormProps;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.AlreadyAliveException;
+import org.apache.storm.generated.AuthorizationException;
+import org.apache.storm.generated.InvalidTopologyException;
+import org.apache.storm.topology.TopologyBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by qxloo on 2017/4/15.
+ */
+@Service
+public class AppMain {
+
+    @Autowired
+    private StormProps stormProps;
+    @Autowired
+    private TopologyBuilder topologyBuilder;
+
+    public void Laugher() throws InvalidTopologyException, AuthorizationException, AlreadyAliveException, InterruptedException {
+        Config config = new Config();
+//        remoteSubmit(stormProps,topologyBuilder,config);
+        localSubmit(stormProps.getTopologyName(),topologyBuilder,config);
+    }
+
+
+    private static void remoteSubmit(StormProps stormProps, TopologyBuilder builder, Config conf)
+            throws InvalidTopologyException, AuthorizationException, AlreadyAliveException {
+        conf.setNumWorkers(stormProps.getTopologyWorkers());
+        conf.setMaxSpoutPending(stormProps.getTopologyMaxSpoutPending());
+        StormSubmitter.submitTopology(stormProps.getTopologyName(), conf, builder.createTopology());
+    }
+
+    /**
+     * 用于debug
+     * @param name
+     * @param builder
+     * @throws InterruptedException
+     */
+    private static void localSubmit(String name,TopologyBuilder builder, Config conf)
+            throws InterruptedException {
+        conf.setDebug(true);
+        conf.setMaxTaskParallelism(3);
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology(name, conf, builder.createTopology());
+        Thread.sleep(100000);
+        cluster.shutdown();
+    }
+}
